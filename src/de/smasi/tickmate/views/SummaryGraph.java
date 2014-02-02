f8251b6f44197ee92fb91b5558fb7a5da69b123c
Hannes Gräuler <hgraeule@uos.de>
2014-02-02 15:53:08 +0100

    Change the summary bar graph to a more pleasing line graph

--- src/de/smasi/tickmate/views/SummaryGraph.java
+++ src/de/smasi/tickmate/views/SummaryGraph.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout.LayoutParams;
@@ -36,7 +38,7 @@ public class SummaryGraph extends View {
 
 	private void init() {
 		setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));		
-		paint = new Paint();
+		paint = new Paint(Paint.ANTI_ALIAS_FLAG );
 		this.data = new LinkedList<Integer>();
 		this.data.add(1);
 		this.data.add(2);
@@ -69,6 +71,7 @@ public class SummaryGraph extends View {
 		paint.setStrokeWidth(0);
 
 		float height = getHeight() - 24.0f;
+		float height0 = height - 26.0f;
 		float width = getWidth();
 
 		// vertical lines
@@ -82,18 +85,50 @@ public class SummaryGraph extends View {
 		paint.setTextSize(18.0f);
 		int len = this.data.size();
 		
+		Path p = new Path();
+		p.moveTo(0, height);
+		float oldH = height;
+		
 		for (int i=0; i < len; i++) {
 			int val = this.data.get(i);
-			float h = ((height-24.f)-val/(1.0f*this.maximum)*(height-24.f)) + 24.0f;
+			float h = (height0-val/(1.0f*this.maximum)*height0) + 26.0f;
+			float x0 = (i)*width/len;
+			float x = (i+0.5f)*width/len;
+			p.cubicTo(x0, oldH, x0, h, x, h);
+			oldH = h;
+		}
+		p.cubicTo(width, oldH, width, height, width, height);
+		
+		paint.setColor(getResources().getColor(android.R.color.holo_blue_dark));
+		paint.setStrokeWidth(2.2f);
+		paint.setStyle(Style.STROKE);
+		canvas.drawPath(p, paint);
+		paint.setStyle(Style.FILL);
+		paint.setColor(getResources().getColor(android.R.color.holo_blue_dark));
+		paint.setAlpha(64);
+		canvas.drawPath(p, paint);
+		paint.setStyle(Style.FILL);
+		paint.setAlpha(255);
+			
+		for (int i=0; i < len; i++) {
+			int val = this.data.get(i);
+			float h = (height0-val/(1.0f*this.maximum)*height0) + 26.0f;
+			float x0 = (i)*width/len;
+			float x = (i+0.5f)*width/len;
 			paint.setStrokeWidth(1);
 			paint.setColor(getResources().getColor(android.R.color.holo_blue_dark));
-			canvas.drawRect(i*width/len, h, (i+1)*width/len, height, paint);
+			//canvas.drawRect(i*width/len, h, (i+1)*width/len, height, paint);
 			if (val > 0) {
 				paint.setColor(getResources().getColor(android.R.color.secondary_text_dark));
-				canvas.drawText(Integer.toString(val), (i+0.5f)*width/len, h-5.0f,paint);
+				canvas.drawText(Integer.toString(val), x, h-9.5f,paint);
+				paint.setColor(getResources().getColor(android.R.color.white));
+				canvas.drawCircle((i+0.5f)*width/len, h, 6.0f, paint);
+				paint.setColor(getResources().getColor(android.R.color.holo_blue_dark));
+				canvas.drawCircle((i+0.5f)*width/len, h, 3.0f, paint);
 			}
 			paint.setColor(getResources().getColor(android.R.color.secondary_text_dark));
 			canvas.drawText(keys.get(i), (i+0.5f)*width/len, height+20.0f,paint);
+			
 		}
 	}
 	
