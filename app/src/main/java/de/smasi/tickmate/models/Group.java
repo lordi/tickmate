0f54f8684cd418b21069813ec6ce2e97348d8b30
Allison Van Pelt <avantpelt@gmail.com>
2015-09-23 18:20:53 -0700

    Added some minor Javadoc.

--- app/src/main/java/de/smasi/tickmate/models/Group.java
+++ app/src/main/java/de/smasi/tickmate/models/Group.java
@@ -21,7 +21,6 @@ package de.smasi.tickmate.models;
 /**
  * Created by js on 8/20/15
  */
-
 public class Group {
 
     public static final Group ALL_GROUP = new Group("All");  // Used to indicate that 'all groups' have been selected for display
@@ -33,51 +32,84 @@ public class Group {
     private boolean mIsSectionHeader = false; // For entries derived from groups.xml to be section headers
 
 
+    /**
+     * @param name group name
+     */
     public Group(String name) {
         mName = name;
     }
 
+    /**
+     * @param name group name
+     * @param description group description
+     */
     public Group(String name, String description) {
         mName = name;
         mDescription = description;
     }
 
+    /**
+     * @return group id
+     */
     public int getId() {
         return mId;
     }
 
+    /**
+     * @param id group id
+     */
     public void setId(int id) {
         mId = id;
     }
 
+    /**
+     * @return group description
+     */
     public String getDescription() {
         return mDescription;
     }
 
+    /**
+     * @param description group description
+     */
     public void setDescription(String description) {
         mDescription = description;
     }
 
+    /**
+     * @return group name
+     */
     public String getName() {
         return mName;
     }
 
+    /**
+     * @param name group name
+     */
     public void setName(String name) {
         mName = name;
     }
 
-
+    /**
+     * @return group name
+     */
     public boolean isSectionHeader() {
         return mIsSectionHeader;
     }
 
-    // Used primarily for debug purposes.
+    /**
+     * Prints contents on Group object. Used primarily for debug purposes.
+     */
     public String toString() {
 //        return "Group:  id(" + mId + ") name(" + mName + ") description(" + mDescription + ")  trackIds(" + TextUtils.join(",", mTrackIds) + ")";
         return "Group:  id(" + mId + ") name(" + mName + ") description(" + mDescription + ")  trackIds(not stored here at the moment)";
     }
 
-    // Only used when parsing groups.xml
+    /**
+     * Only used when parsing groups.xml.
+     *
+     * @param sectionHeader group name
+     */
     public void setSectionHeader(boolean sectionHeader) {
         mIsSectionHeader = sectionHeader;
     }
