4184f7bc7de7ce9755c5356619e543091b3bc4e2
Marcelo Medeiros Eler <marceloeler@gmail.com>
2017-04-21 14:16:27 +0100

    labeling icons and colors

--- app/src/main/java/de/smasi/tickmate/views/IconListAdapter.java
+++ app/src/main/java/de/smasi/tickmate/views/IconListAdapter.java
@@ -59,6 +59,9 @@ public class IconListAdapter extends BaseAdapter {
 		int icon_id = r.getIdentifier((String)getItem(position), "drawable", context.getPackageName());
 
         imageView.setImageResource(icon_id);
+        imageView.setContentDescription(icon_names.get(position));
+        //imageView.setMinimumWidth(100);
+        //imageView.setMinimumHeight(100);
         return imageView;
     }
 
