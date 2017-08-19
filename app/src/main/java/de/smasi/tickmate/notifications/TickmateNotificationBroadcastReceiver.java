637cabf8853635d5f27af77122d29a45bc8c178f
Hannes Gräuler <graeuler@geoplex.de>
2017-08-19 14:24:18 +0200

    Add Alarm only when preference is set

--- app/src/main/java/de/smasi/tickmate/notifications/TickmateNotificationBroadcastReceiver.java
+++ app/src/main/java/de/smasi/tickmate/notifications/TickmateNotificationBroadcastReceiver.java
@@ -4,8 +4,11 @@ import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.preference.Preference;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -20,33 +23,46 @@ public class TickmateNotificationBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "Alarm received");
-        Locale locale = Locale.getDefault();
 
-        java.text.DateFormat dateFormat = android.text.format.DateFormat
-                .getDateFormat(context);
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.glyphicons_054_clock_white)
-                        .setContentTitle("Tick reminder")
-                        .setAutoCancel(true)
-                        .setContentText("Tap to enter your ticks for " +
-                                Calendar.getInstance().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, locale)
-                        );
-        Intent resultIntent = new Intent(context, Tickmate.class);
+        Boolean notifyMe = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notify-me", false);
+
+        Log.d(TAG, "Alarm received; notifyMe=" + notifyMe.toString());
+
+        if (notifyMe) {
+            Locale locale = Locale.getDefault();
+
+            java.text.DateFormat dateFormat = android.text.format.DateFormat
+                    .getDateFormat(context);
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.glyphicons_054_clock_white)
+                            .setContentTitle("Tick reminder")
+                            .setAutoCancel(true)
+                            .setContentText("Tap to enter your ticks for " +
+                                    Calendar.getInstance().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, locale)
+                            );
+            Intent resultIntent = new Intent(context, Tickmate.class);
 
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(pendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            mBuilder.setContentIntent(pendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 // mId allows you to update the notification later on.
-        mNotificationManager.notify(0, mBuilder.build());
+            mNotificationManager.notify(0, mBuilder.build());
+        }
     }
 
     public static void activateAlarm(Context context)
     {
-        Log.i(TAG, "Setting alarm at 12 o clock...");
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(Calendar.HOUR_OF_DAY, 15);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        Log.i(TAG, "Setting alarm at: " + cal.toString());
 
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
@@ -56,7 +72,7 @@ public class TickmateNotificationBroadcastReceiver extends BroadcastReceiver {
         // cancel any previous alarms
         am.cancel(pi);
 
-        //After after 30 seconds
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, pi);
+        //TODO use setInexactRepeating here
+        am.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, pi);
     }
 }
