30f0211984afb82ee0a1283f79f88f527a47d0af
Hannes Gräuler <graeuler@geoplex.de>
2017-08-19 21:40:47 +0200

    Set correct time in Receiver, use inexact alarms, repeat daily

--- app/src/main/java/de/smasi/tickmate/notifications/TickmateNotificationBroadcastReceiver.java
+++ app/src/main/java/de/smasi/tickmate/notifications/TickmateNotificationBroadcastReceiver.java
@@ -12,6 +12,8 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -24,11 +26,13 @@ public class TickmateNotificationBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Boolean notifyMe = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notify-me", false);
+        Boolean enabled = PreferenceManager.getDefaultSharedPreferences(context)
+                                           .getBoolean("notification-enabled", false);
 
-        Log.d(TAG, "Alarm received; notifyMe=" + notifyMe.toString());
+        Log.d(TAG, "Alarm received; enabled=" + enabled.toString());
 
-        if (notifyMe) {
+        if (enabled) {
+
             Locale locale = Locale.getDefault();
 
             java.text.DateFormat dateFormat = android.text.format.DateFormat
@@ -56,13 +60,22 @@ public class TickmateNotificationBroadcastReceiver extends BroadcastReceiver {
     public static void activateAlarm(Context context)
     {
         Calendar cal = Calendar.getInstance();
+        java.text.DateFormat timeFmt = java.text.DateFormat.getTimeInstance(java.text.DateFormat.SHORT);
 
-        cal.set(Calendar.HOUR_OF_DAY, 15);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
+        Boolean enabled = PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean("notification-enabled", false);
 
-        Log.i(TAG, "Setting alarm at: " + cal.toString());
+        String timeString = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString("notification-time", "");
+
+        try {
+            cal.setTime(timeFmt.parse(timeString));
+        } catch (ParseException e) {
+            Log.w(TAG, "Error parsing time: " + timeString);
+            enabled = false;
+        }
+
+        Log.d(TAG, "Updating alarm; enabled=" + enabled.toString() + ", time=" + timeString);
 
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
@@ -72,7 +85,26 @@ public class TickmateNotificationBroadcastReceiver extends BroadcastReceiver {
         // cancel any previous alarms
         am.cancel(pi);
 
-        //TODO use setInexactRepeating here
-        am.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, pi);
+        if (enabled) {
+
+            Calendar alarmTime = Calendar.getInstance();
+            alarmTime.set(Calendar.HOUR_OF_DAY, cal.get(Calendar.HOUR_OF_DAY));
+            alarmTime.set(Calendar.MINUTE, cal.get(Calendar.MINUTE));
+            alarmTime.set(Calendar.SECOND, 0);
+            alarmTime.set(Calendar.MILLISECOND, 0);
+
+            if (alarmTime.before(Calendar.getInstance())) {
+                alarmTime.add(Calendar.DAY_OF_YEAR, 1);
+            }
+
+            java.text.DateFormat dateFmt = java.text.DateFormat.getDateTimeInstance(
+                    java.text.DateFormat.MEDIUM, java.text.DateFormat.LONG);
+
+            Log.i(TAG, "Setting alarm at: " + dateFmt.format(alarmTime.getTime()));
+
+            am.setInexactRepeating(AlarmManager.RTC_WAKEUP, alarmTime.getTimeInMillis(),
+                                   AlarmManager.INTERVAL_DAY, pi);
+        }
+
     }
 }
